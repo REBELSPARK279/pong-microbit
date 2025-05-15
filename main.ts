53e6a7bb485af7a24ae28b88de700ce764caa276
REBELSPARK279 <130930743+REBELSPARK279@users.noreply.github.com>
2025-05-15 17:18:30 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -99,6 +99,8 @@ let mode = 0;
 mode 0 --> menu
 mode 1 --> 1p game
 mode 2 --> server selection
+
+mode 279 --> doNothing
 */
 let menuCount = 1;
 let serverPage = 0;
@@ -127,11 +129,25 @@ input.onButtonPressed(Button.B, function () {
 });
 input.onButtonPressed(Button.AB, function () {
     if (mode == 0) { //mainMenu
-        if (menuCount == 2) {
+        if (menuCount == 1) {
+            mode = 279;
+            basic.clearScreen();
+            basic.pause(600);
+            for (let temp = 3; temp > 1; temp++) {
+                setNumber(temp);
+                basic.pause(1000);  
+            }
+            basic.clearScreen();
+            mode = 1;
+        } else if (menuCount == 2) {
             mode = 2;
             basic.clearScreen();
             basic.pause(350);
             setNumber(serverPage);
         }
     }
+});
+
+basic.forever(function() {
+     
 });
