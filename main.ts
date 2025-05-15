fd0af7a5bd064a890f253ec260af00961adfa327
REBELSPARK279 <130930743+REBELSPARK279@users.noreply.github.com>
2025-05-15 16:35:02 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,137 @@
+//functions
+
+function setNumber(number: any) {
+    if (number == 0) {
+        basic.showLeds(`
+        .###.
+        .#.#.
+        .#.#.
+        .#.#.
+        .###.
+        `);
+    } else if (number == 1) {
+        basic.showLeds(`
+        ..#..
+        .##..
+        ..#..
+        ..#..
+        .###.
+        `);
+    } else if (number == 2) {
+        basic.showLeds(`
+        .###.
+        ...#.
+        .###.
+        .#...
+        .###.
+        `);
+    } else if (number == 3) {
+        basic.showLeds(`
+        .###.
+        ...#.
+        .###.
+        ...#.
+        .###.
+        `);
+    } else if (number == 4) {
+        basic.showLeds(`
+        .#.#.
+        .#.#.
+        .###.
+        ...#.
+        ...#.
+        `);
+    } else if (number == 5) {
+        basic.showLeds(`
+        .###.
+        .#...
+        .###.
+        ...#.
+        .###.
+        `);
+    } else if (number == 6) {
+        basic.showLeds(`
+        .###.
+        .#...
+        .###.
+        .#.#.
+        .###.
+        `);
+    } else if (number == 7) {
+        basic.showLeds(`
+        .###.
+        .#.#.
+        ...#.
+        ..#..
+        ..#..
+        `);
+    } else if (number == 8) {
+        basic.showLeds(`
+        .###.
+        .#.#.
+        .###.
+        .#.#.
+        .###.
+        `);
+    } else if (number == 9) {
+        basic.showLeds(`
+        .###.
+        .#.#.
+        .###.
+        ...#.
+        .###.
+        `);
+    } else if (number == 10) {
+        basic.showLeds(`
+        #.###
+        #.#.#
+        #.#.#
+        #.#.#
+        #.###
+        `);
+    }
+}
+
+//var
+
+let mode = 0;
+/* 
+mode 0 --> menu
+mode 1 --> 1p game
+mode 2 --> server selection
+*/
+let menuCount = 1;
+let serverPage = 0;
+
+//startup
+
+setNumber(1);
+
+//inputs
+
+input.onButtonPressed(Button.A, function () {
+    if (mode == 0) { //mainMenu
+        if (menuCount != 1) {
+            menuCount--;
+            setNumber(menuCount);
+        }
+    }
+});
+input.onButtonPressed(Button.B, function () {
+    if (mode == 0) { //mainMenu
+        if (menuCount != 2) {
+            menuCount++;
+            setNumber(menuCount);
+        }
+    }
+});
+input.onButtonPressed(Button.AB, function () {
+    if (mode == 0) { //mainMenu
+        if (menuCount == 2) {
+            mode = 2;
+            basic.clearScreen();
+            basic.pause(350);
+            setNumber(serverPage);
+        }
+    }
+});
